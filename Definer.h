9d5ec48703daacc3149ec0cf587799da957275f0
EasierMonk95 <58497916+EasierMonk95@users.noreply.github.com>
2020-05-15 02:26:30 -0500

    Update Definer.h

--- Definer.h
+++ Definer.h
@@ -5,16 +5,13 @@
 
 using namespace std;
 
-void seleccionar();
-void metodo1(bool metodo, unsigned long long int semilla);
 string leer_Txt(bool metodo, string text);
 void text2bin(string texto,bool *cod);
-void separacion(unsigned long long int semilla, bool *cod, unsigned long long int tam, bool codify);
-unsigned short reglas(short *seg, bool *data, unsigned short regla, unsigned long long int semilla, bool codify);
 string bin2text(string texto,bool *cod);
 void escribir_Txt(string texto, bool metodo);
 long long int tamano(char *name);
 int mover(short *seg, unsigned long long int semilla, bool *data);
 int moverdevuelta(short *seg,unsigned long long int semilla, bool *data);
 string metodo2(bool metodo, unsigned long long int semilla, string text);
+string metodo2mod(bool metodo, unsigned long long int semilla, string text);
 void separacion2(unsigned long long int semilla, bool *cod, unsigned long long int tam, bool metodo);
